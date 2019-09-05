b15500a1f569a40064e516fe5ff04a923e83e839
Steffen Kroggel <kroggel@rkw.de>
2019-09-05 09:46:55 +0200

    [FIX] extended security question

--- Classes/Deployment.php
+++ Classes/Deployment.php
@@ -47,7 +47,7 @@ class Deployment
         }
         
         // security question
-        $question = new \Symfony\Component\Console\Question\ConfirmationQuestion('Continue with deployment of branch [' . $this->options['branch'] . '] to [' . $this->options['workspacesBasePath'] . '] on server [' . $this->options['hostname'] . "]?\n(y|n) ", false, '/^(y|j)/i');
+        $question = new \Symfony\Component\Console\Question\ConfirmationQuestion('Continue with deployment of branch [' . $options['branch'] . '] to [' . $options['deploymentPath'] . '] on server [' . $options['hostname'] . "]?\n(y|n) ", false, '/^(y|j)/i');
         $helper = new \Symfony\Component\Console\Helper\QuestionHelper;
         $input = new \Symfony\Component\Console\Input\ArgvInput;
         $output = new \Symfony\Component\Console\Output\StreamOutput(fopen('php://stdout', 'w'));
